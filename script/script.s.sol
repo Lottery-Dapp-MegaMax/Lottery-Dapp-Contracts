69814f868bca85cfd076fae858e656ef06010842
hoangquocvietuet <hoangquocvietuet@gmail.com>
2024-03-14 20:30:08 +0700

    updated contracts

--- script/script.s.sol
+++ script/script.s.sol
@@ -17,7 +17,8 @@ contract DeployScript is Script {
         conflux_testnet_usdt_contract = IERC20(0x7d682e65EFC5C13Bf4E394B8f376C48e6baE0355);
         myPoolManager = new PoolManager();
         console.log("myPoolManager created at: %s", address(myPoolManager));
-        // MyPool myPool = new MyPool(address(myPoolManager), conflux_testnet_usdt_contract);
+        // MyPool myPool = new MyPool(0x0bBF172103A6C9eA699c391FF63ffDE41C2c28C3, conflux_testnet_usdt_contract);
+        // console.log("myPool created at: %s", address(myPool));
         // myPoolManager.addNewPool(address(myPool), 300);
         // MyPool myPool2 = new MyPool(address(myPoolManager), conflux_testnet_usdt_contract);
         // myPoolManager.addNewPool(address(myPool2), 60);
